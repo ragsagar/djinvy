918bcb0eea3a1c897db6e3f4932ebe942f0822e2
Rag Sagar.V <ragsagar@gmail.com>
2015-03-24 17:08:02 +0400

    Activating inventory urls

--- djinvy/urls.py
+++ djinvy/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'djinvy.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^inventory/', include('inventory.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
