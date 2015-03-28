1aad365e9d27c0150dcb71b02d452348ad1d970f
Rag Sagar.V <ragsagar@gmail.com>
2015-03-28 14:41:17 +0400

    Adding static settings

--- djinvy/settings.py
+++ djinvy/settings.py
@@ -85,3 +85,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'assets'),
+    )
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
